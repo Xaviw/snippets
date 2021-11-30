34504831ff4fb6fec6b6dcd0768d12f82453def2
XRain <704466069@qq.com>
2021-11-30 22:51:36 +0800

    小改动

--- myCall.js
+++ myCall.js
@@ -1,15 +1,15 @@
-Function.prototype.myCall = function (context) {
+Function.prototype.myCall = function (context, ...args) {
   // 处理传入null的情况
   context = context || window;
   // this为调用myCall的函数
   let fn = Symbol();
   context[fn] = this;
   // 得到参数
-  context[fn](...Array.from(arguments).slice(1));
-  console.log(context);
+  context[fn](...args);
   // 删除添加的fn
   delete context[fn];
 };
+
 var value = 2;
 var obj = { value: 1 };
 function test(a, b) {
--- 常用方法.js
+++ 常用方法.js
@@ -1,7 +1,7 @@
 function DeepClone (obj) {
     if (obj == null || typeof obj !== 'object') return obj
     if (obj instanceof Date) return new Date(obj)
-    if (obj instanceof RegExp) return new RegExp(obj)
+    if (obj instanceof RegExp) return new RegExp(obj)~
     let result = new obj.constructor()
     for (let key in obj) {
         if (obj.hasOwnProperty(key)) {
